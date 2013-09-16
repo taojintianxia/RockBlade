56156349f23bd6092018b55ecaf8748e94630a3c
Kane.Sun <taojintianxia@126.com>
2013-09-16 12:59:01 +0800

    add new method to get top n amount stock

--- src/com/rockblade/calculatecenter/Calculator.java
+++ src/com/rockblade/calculatecenter/Calculator.java
@@ -1,9 +1,12 @@
 package com.rockblade.calculatecenter;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.rockblade.cache.StockCache;
 import com.rockblade.invoker.ParserInvoker;
 import com.rockblade.model.Stock;
 import com.rockblade.util.StockUtil;
@@ -16,28 +19,28 @@ import com.rockblade.util.StockUtil;
  * 
  */
 
-/**
- * 
- * 
- * @author Kane.Sun
- * 
- */
 public class Calculator {
 
 	public static void main(String... args) {
 
 	}
 
-	public static List<Stock> getTopNStockAmount(int n) {
-		new ParserInvoker();
-
-		Map<String, Long> allStocks = new LinkedHashMap<>();
-		StockUtil.sortMapByValue(allStocks);
-
-		for (Map.Entry<String, Long> entry : allStocks.entrySet()) {
-			System.out.println(entry.getKey() + "--->" + entry.getValue());
+	public static List<Stock> getTopNSHStockAmount(int n) {
+		ParserInvoker parserInvoker = new ParserInvoker();
+		Map<String, Stock> currentSHStockMap = StockCache.getCurrentSHStockMap();
+		Map<String, Double> currentSHStockAmountMap = new LinkedHashMap<>(currentSHStockMap.size());
+		List<Stock> topNSHStockAmountList = new ArrayList<>(n);
+		for (Map.Entry<String, Stock> entry : currentSHStockMap.entrySet()) {
+			currentSHStockAmountMap.put(entry.getKey(), entry.getValue().getAmount());
 		}
 
-		return null;
+		StockUtil.sortMapByValue(currentSHStockAmountMap);
+
+		Iterator<String> it = currentSHStockAmountMap.keySet().iterator();
+		for (int i = 0; i < n; i++) {
+			topNSHStockAmountList.add(currentSHStockMap.get(it.next()));
+		}
+
+		return topNSHStockAmountList;
 	}
 }
